a2e4567351149863db92a6c909c02eb097d5364f
strboul <me@strboul.com>
2022-11-03 22:07:11 +0000

    Deploying to gh-pages from @ strboul/kwic-ts@683d07ffd95fbce6098e6cfa6a5b429fa21b0454 🚀

---
+++ _next/static/zbgNNTwtaC0OgQDX5l2-T/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/708-956fe884a5444b84492b.js","static/chunks/pages/index-a925a2c425006356c132.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
